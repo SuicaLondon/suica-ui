66d087b08127aefa8584b3305cf3182ec091ed1d
SuicaDavid <suicadevinlondon@gmail.com>
2024-04-04 23:34:19 +0100

    feat: complete the switch button

--- src/toggle-button/toggle-button.tsx
+++ src/toggle-button/toggle-button.tsx
@@ -23,15 +23,21 @@ export function ToggleButton({
 			/>
 			<div
 				className={cn(
-					'relative w-11 h-6 bg-gray-200 rounded-full',
-					'peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300',
-					'peer-checked:after:translate-x-full peer-checked:after:border-white',
-					'peer-checked:bg-primary-dart peer-checked:after:bg-white',
+					'relative w-14 h-6 bg-gray-200 rounded-full flex items-center',
 					'after:border after:rounded-full after:h-5 after:w-5',
 					'after:content-[""] after:absolute after:top-[2px] after:start-[2px]',
 					'after:bg-primary-dart after:border-gray-300 after:transition-all',
+					'peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300',
+					'peer-checked:after:translate-x-8', // Should be width - height
+					'peer-checked:bg-primary-dart peer-checked:after:bg-white peer-checked:after:border-white',
+					{
+						'justify-start text-white': checked,
+						'justify-end text-primary-dart': !checked,
+					},
 				)}
-			/>
+			>
+				<p className="px-2 text-sm">{checked ? 'On' : 'Off'}</p>
+			</div>
 		</label>
 	)
 }
