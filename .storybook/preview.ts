25f1354f90e262903f41b5c6ad8357370b3c3757
SuicaLondon <suicadevinlondon@gmail.com>
2024-08-31 00:07:29 +0100

    chore: config the viewport and sidebar

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,40 @@
-import type { Preview } from "@storybook/react";
-import { withThemeByClassName } from "@storybook/addon-themes";
+import { DEFAULT_VIEWPORT, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+import type { Preview } from '@storybook/react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-import "../src/output.css";
+import '../src/output.css'
+
+const CUSTOM_VIWQPORTS = {
+	smallDesktop: {
+		name: 'Small Desktop',
+		styles: {
+			width: '1024px',
+			height: '768px',
+		},
+		type: 'desktop',
+	},
+	normalDesktop: {
+		name: 'Normal Desktop',
+		styles: {
+			width: '1920px',
+			height: '1080px',
+		},
+		type: 'desktop',
+	},
+}
 
 export const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-  },
-};
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		viewport: {
+			viewports: {
+				...MINIMAL_VIEWPORTS,
+				...INITIAL_VIEWPORTS,
+				...CUSTOM_VIWQPORTS,
+			},
+			defaultViewport: 'customDesktop', // Optional: Set a global default viewport
+		},
+	},
+}
 
-export default preview;
+export default preview
