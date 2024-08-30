49f9adedef83b65c69ac43659b8da47e3bf1be51
SuicaLondon <suicadevinlondon@gmail.com>
2024-08-31 00:41:49 +0100

    feat: update the sidebar button

--- src/sidebar/sidebar-button.tsx
+++ src/sidebar/sidebar-button.tsx
@@ -1,26 +1,51 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
+import clsx from 'clsx'
 
-export function SidebarButton() {
+type SidebarButtonProps = {
+	isOpened: boolean
+	setIsOpened: (toOpened: boolean) => void
+}
+
+export function SidebarButton({ isOpened, setIsOpened }: SidebarButtonProps) {
 	return (
 		<button
 			aria-controls="default-sidebar"
 			type="button"
-			className="ms-3 mt-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 sm:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+			className={clsx(
+				'cursor-pointer w-6 h-6 translate-x-0',
+				'fixed phone:left-2 phone:top-2 left-4 top-4 z-999',
+				'flex flex-col justify-center items-center',
+			)}
+			onClick={() => setIsOpened(!isOpened)}
 		>
 			<span className="sr-only">Open sidebar</span>
-			<svg
-				className="h-6 w-6"
-				aria-hidden="true"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					clipRule="evenodd"
-					fillRule="evenodd"
-					d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-				/>
-			</svg>
+			<span
+				className={twMerge(
+					'block w-6 h-[2px] rounded bg-primary-gray transition-all origin-center',
+					clsx({
+						' w-[calc(16px_/_sin(45deg))] bg-primary-gray rotate-45 -mb-[1px]':
+							isOpened,
+					}),
+				)}
+			></span>
+			<span
+				className={twMerge(
+					'block w-6 h-[2px] rounded bg-primary-gray transition-all origin-center my-2',
+					clsx({
+						'w-[calc(16px_/_sin(45deg))] bg-primary-gray hidden my-0': isOpened,
+					}),
+				)}
+			></span>
+			<span
+				className={twMerge(
+					'block w-6 h-[2px] rounded bg-primary-gray transition-all origin-center',
+					clsx({
+						'w-[calc(16px_/_sin(45deg))] bg-primary-gray -rotate-45 -mt-[1px]':
+							isOpened,
+					}),
+				)}
+			></span>
 		</button>
 	)
 }
