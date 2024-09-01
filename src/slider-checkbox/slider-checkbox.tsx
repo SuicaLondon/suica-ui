69ec4f67e74a6f0c76538ba1d2de92f96aa917bf
SuicaLondon <66697085+SuicaLondon@users.noreply.github.com>
2024-09-01 18:19:27 +0100

    chore: completed the docs of the slider checkbox (#5)

--- src/slider-checkbox/slider-checkbox.tsx
+++ src/slider-checkbox/slider-checkbox.tsx
@@ -17,15 +17,15 @@ export function SliderCheckbox<T>({
 		<input
 			type="range"
 			className={clsx(
-				'h-4 m-1 appearance-none bg-transparent cursor-pointer disabled:opacity-50 disabled:pointer-events-none focus:outline-none',
-				'[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:-mt-1 [&::-webkit-slider-thumb]:rounded-full',
+				'm-1 h-4 cursor-pointer appearance-none bg-transparent focus:outline-none disabled:pointer-events-none disabled:opacity-50',
+				'[&::-webkit-slider-thumb]:-mt-1 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full',
 				'[&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:shadow-slider-thumb',
 				'[&::-webkit-slider-runnable-track]:w-full [&::-webkit-slider-runnable-track]:rounded-full',
-				'[&::-webkit-slider-runnable-track]:h-2 [&::-webkit-slider-runnable-track]:bg-gray-800',
-				'[&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full',
-				'[&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-4 [&::-moz-range-thumb]:border-primary-gray',
-				'[&::-moz-range-track]:w-full [&::-moz-range-track]:h-2 [&::-moz-range-track]:rounded-full',
-				'[&::-moz-range-track]:bg-gray-800',
+				'[&::-webkit-slider-runnable-track]:h-2 [&::-webkit-slider-runnable-track]:bg-primary-gray',
+				'[&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:rounded-full',
+				'[&::-moz-range-thumb]:border-4 [&::-moz-range-thumb]:border-primary-gray [&::-moz-range-thumb]:bg-white',
+				'[&::-moz-range-track]:h-2 [&::-moz-range-track]:w-full [&::-moz-range-track]:rounded-full',
+				'[&::-moz-range-track]:bg-primary-gray',
 			)}
 			min={0}
 			max={values.length - 1}
